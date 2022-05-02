b872b1c1aa3537ccc0021fbadb2fc089436bc8b9
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Pro.local>
2022-05-02 13:38:58 +0530

    Gcp project change

--- src/main/java/com/example/demo/GcpStorageDemo.java
+++ src/main/java/com/example/demo/GcpStorageDemo.java
@@ -1,13 +1,9 @@
 package com.example.demo;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.ByteBuffer;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.ReadChannel;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 
 @RestController
 @RequestMapping("/gcp")
@@ -32,10 +27,7 @@ public class GcpStorageDemo {
 	@GetMapping("send-data")
 	public String sendData() throws IOException {
 
-//		GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("/Users/abhisheksingh/Downloads/gcp-image-hosting-55f85a03a0e4.json"));
-//
-//		 Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-		BlobId id = BlobId.of("storage-java", "demofile.txt");
+		BlobId id = BlobId.of("file-storage-demo", "demofile.txt");
 		BlobInfo info = BlobInfo.newBuilder(id).build();
 
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -44,18 +36,29 @@ public class GcpStorageDemo {
 		if (resource == null) {
 			throw new IllegalArgumentException("file is not found!");
 		} else {
-			System.out.println("before service account::::");
 
 			InputStream serviceAccount = new ClassPathResource("demofile.txt").getInputStream();
 			// File file =new File(resource.getFile());
 			byte[] array = IOUtils.toByteArray(serviceAccount);
-			
-			System.out.println("byte array ::::");
-
-			// byte[] array= serviceAccount.readAllBytes();
-			// byte[] array=Files.readAllBytes(Paths.get(file.toURI()));
 			storage.create(info, array);
 		}
 		return "File uploaded Successfully";
+	}
+
+	@GetMapping("get-data")
+	public String getData() throws IOException {
+		StringBuffer sb = new StringBuffer();
+
+		try (ReadChannel channel = storage.reader("file-storage-demo", "demofile.txt")) {
+			ByteBuffer byteBuffer = ByteBuffer.allocate(64 * 1024);
+			while (channel.read(byteBuffer) > 0) {
+				byteBuffer.flip();
+				String data = new String(byteBuffer.array(), 0, byteBuffer.limit());
+				sb.append(data);
+				byteBuffer.clear();
+
+			}
+			return sb.toString();
+		}
 	}
 }
